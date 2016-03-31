729084b435458a1fdfd22350decb874b6fa9450c
Julien Semaan <jul.semaan@gmail.com>
2016-03-31 07:55:25 -0400

    naming cleanup

--- recording_queue.go
+++ recording_queue.go
@@ -5,34 +5,34 @@ import (
 )
 
 type RecordingQueue struct {
-	destinationsQueue      []*Destination
-	destinationsQueueMutex *sync.Mutex
+	queue      []*Destination
+	queueMutex *sync.Mutex
 }
 
 func (self *RecordingQueue) push(destination *Destination) {
-	self.destinationsQueueMutex.Lock()
-	self.destinationsQueue = append(self.destinationsQueue, destination)
-	self.destinationsQueueMutex.Unlock()
+	self.queueMutex.Lock()
+	self.queue = append(self.queue, destination)
+	self.queueMutex.Unlock()
 }
 
 func (self *RecordingQueue) shift() *Destination {
-	self.destinationsQueueMutex.Lock()
+	self.queueMutex.Lock()
 	var destination *Destination
-	if len(self.destinationsQueue) > 0 {
-		destination, self.destinationsQueue = self.destinationsQueue[0], self.destinationsQueue[1:]
+	if len(self.queue) > 0 {
+		destination, self.queue = self.queue[0], self.queue[1:]
 	}
-	self.destinationsQueueMutex.Unlock()
+	self.queueMutex.Unlock()
 	return destination
 }
 
 func (self *RecordingQueue) empty() bool {
-	return len(self.destinationsQueue) == 0
+	return len(self.queue) == 0
 }
 
 func NewRecordingQueue() *RecordingQueue {
 	recording_queue := &RecordingQueue{}
-	recording_queue.destinationsQueue = make([]*Destination, 0)
-	recording_queue.destinationsQueueMutex = &sync.Mutex{}
+	recording_queue.queue = make([]*Destination, 0)
+	recording_queue.queueMutex = &sync.Mutex{}
 	return recording_queue
 }
 
